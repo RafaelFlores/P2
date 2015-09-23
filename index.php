fce0147437ad4a07b8697ea114859b2e73def555
Rafael Flores <rafloresgo@gmail.com>
2015-09-23 15:49:03 -0500

    itro message

--- index.php
+++ index.php
@@ -13,7 +13,14 @@
   </head>
   <body>
 <h1 class="titleBox"> Password Generator</h1>
-
+<p class = centerBox>
+  A password needs to comply with two functions: It has to be hard to crack but
+  it has to be easy to remember, so thats why  this page genereates passwords
+  using words that are easy to remember. This passwords are known to be
+  xkcd passwords because they explain the issue in their comic #936 Password
+  Strength at <a href="http://www.explainxkcd.com/wiki/index.php/936:_Password_Strength"
+  >http://www.explainxkcd.com/wiki/index.php/936:_Password_Strength</a>
+</p>
 <br>
 <form  class = "centerBox" action="index.php" method="post">
   Number of Words max(<?php echo $maxNumOfWords ?>): <input type="text" name="numOfWords"> <br>
